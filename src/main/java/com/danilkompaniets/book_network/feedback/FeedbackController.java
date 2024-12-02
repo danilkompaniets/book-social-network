3a671b34717b365d3bdd5c231335c214541671e1
danilkompaniets <kompaniets1592925@gmail.com>
2024-12-02 20:13:46 +0300

    feedback functionality added

---
+++ src/main/java/com/danilkompaniets/book_network/feedback/FeedbackController.java
@@ -0,0 +1,34 @@
+package com.danilkompaniets.book_network.feedback;
+
+
+import com.danilkompaniets.book_network.common.PageResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("feedbacks")
+@RequiredArgsConstructor
+@Tag(name = "Feedback")
+public class FeedbackController {
+    private final FeedbackService feedbackService;
+
+    @PostMapping
+    public ResponseEntity<Integer> saveFeedback(@Valid @RequestBody FeedbackRequest request, Authentication connectedUser) {
+        return ResponseEntity.ok(feedbackService.save(request, connectedUser));
+    }
+
+    @GetMapping("/book/{book-id}")
+    public ResponseEntity<PageResponse<FeedbackResponse>> findAllFeedbacksByBook(
+            @PathVariable("book-id") Integer bookId,
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(feedbackService.findAllFeedbacksByBook(connectedUser, bookId, page, size, connectedUser));
+    }
+
+}
---
+++ src/main/java/com/danilkompaniets/book_network/feedback/FeedbackMapper.java
@@ -0,0 +1,32 @@
+package com.danilkompaniets.book_network.feedback;
+
+import com.danilkompaniets.book_network.book.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+
+    private final FeedbackService feedbackService;
+
+    public FeedbackMapper(FeedbackService feedbackService) {
+        this.feedbackService = feedbackService;
+    }
+
+    public Feedback toFeedback(FeedbackRequest request) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder().id(request.bookId()).build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback f, Integer userId) {
+        return FeedbackResponse.builder()
+                .note(f.getNote())
+                .comment(f.getComment())
+                .ownFeedback(Objects.equals(f.getCreatedBy(), userId))
+                .build();
+    }
+}
